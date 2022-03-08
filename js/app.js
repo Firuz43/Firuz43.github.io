4342ccc12c5958292b8e993e2b49a555cebdce5e
Firuz43 <firuz1199@mail.ru>
2022-03-08 08:05:36 +0400

    burger menu

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+// Burger menu
+const burger = document.querySelector('#burger-menu');
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+});
+
+
+// clicking links remove burger menu
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
