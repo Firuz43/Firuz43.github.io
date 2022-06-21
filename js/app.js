b519cfc82a746c6beee2db9fcce0b1fc8c6fd5d6
Firuz43 <firuz1199@mail.ru>
2022-06-20 23:08:09 -0700

    changes

--- js/app.js
+++ js/app.js
@@ -6,3 +6,16 @@ function toggleButton() {
 }
 
 hamburgerButton.addEventListener('click', toggleButton)
+
+
+function changeBg() {
+    var navbar = document.getElementById('navbar');
+    var scrollValue = window.scrollY;
+    if(scrollY < 350) {
+        navbar.classList.remove('bgColor');
+    }else {
+        navbar.classList.add('bgColor');
+    }
+}
+
+window.addEventListener('scroll', changeBg);
